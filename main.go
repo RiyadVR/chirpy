f4426f6a15c557b4f4443c1a4eed57c52613fe70
RiyadVR <riyadvrasheed.official@gmail.com>
2025-09-26 20:20:23 +0400

    first commit & created a tiny server in go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Error")
+	}
+}
