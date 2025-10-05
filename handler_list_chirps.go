69c7642d5a2336d29818a3ae358b3aaf696226cb
RiyadVR <riyadvrasheed.official@gmail.com>
2025-10-06 01:38:40 +0400

    updated GET /api/chirps to accept author_id query parameter

--- handler_list_chirps.go
+++ handler_list_chirps.go
@@ -2,9 +2,43 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerListChirps(w http.ResponseWriter, r *http.Request) {
+
+	authorID := r.URL.Query().Get("author_id")
+
+	if authorID != "" { // this block means the author_id is present the query parameters
+		authorUUID, err := uuid.Parse(authorID)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid UUID", err)
+			return
+		}
+
+		dbChirps, err := cfg.db.GetChirpByUserID(r.Context(), authorUUID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
+
+		chirps := []Chirp{}
+		for _, dbChirp := range dbChirps {
+			chirps = append(chirps, Chirp{
+				ID:        dbChirp.ID,
+				CreatedAt: dbChirp.CreatedAt,
+				UpdatedAt: dbChirp.UpdatedAt,
+				UserID:    dbChirp.UserID,
+				Body:      dbChirp.Body,
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, chirps)
+		return
+
+	}
+
 	dbChirps, err := cfg.db.ListChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
